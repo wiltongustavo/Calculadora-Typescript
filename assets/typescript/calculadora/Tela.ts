e4c5cc0d19889802c20b7c2abf6e7b33ba4f9dfd
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2024-06-04 22:24:56 -0300

    comite do projeto

---
+++ assets/typescript/calculadora/Tela.ts
@@ -0,0 +1,21 @@
+export default class Tela {
+    constructor(
+        private elemento: HTMLDivElement | null = document.querySelector("#values")
+    ){
+        this.conteudo = "0"
+    }
+
+    set conteudo(valor: string){
+        if(valor.toString().length > 12){
+            valor = "error";
+        }
+        if(this.elemento){
+            this.elemento.innerHTML = valor.toString().replace(".", ",");
+        }
+    }
+
+    get conteudo(): string{
+        return this.elemento ? this.elemento.innerHTML : "0"
+        
+    }
+}
